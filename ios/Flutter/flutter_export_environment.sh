6deb72f519836ce163cbf152e72e50385493b392
leeyongit <leeyongit@163.com>
2025-05-14 14:09:05 +0800

    fix ios

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\bot\flutter_flexible"
+export "FLUTTER_ROOT=/Users/liyong/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/work/Codeup/flutter/flutter_flexible"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=/Volumes/work/Codeup/flutter/flutter_flexible/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Volumes/work/Codeup/flutter/flutter_flexible/.dart_tool/package_config.json"
